7e229dfec235be9af7564add65bc390cde498d24
aleks <aleksei-davletov@mail.ru>
2023-06-26 12:44:15 +0300

    Выполнил домашнюю работу

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задание 1");
+        String firstName = "Ivan", lastName = "Ivanov", middleName = "Ivanovich",
+                fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    public static void task2() {
+        System.out.println("Задание 2");
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    }
+
+    public static void task3() {
+        System.out.println("Задание 3");
+        String fullName = "Иванов Семён Семёнович";
+        String fullNameTrue = fullName.replace("ё", "е");
+        fullNameTrue = fullNameTrue.replace("Ё" , "Е");
+        System.out.println("Данные ФИО сотрудника — " + fullNameTrue);
     }
 }
